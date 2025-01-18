05ef27b265d64bf8a9e25bea7eef86ad29a816e3
minakdr <mina125.ak@gmail.com>
2025-01-18 01:22:27 +0100

    qsq

--- src/App.js
+++ src/App.js
@@ -1,23 +1,12 @@
-import logo from './logo.svg';
 import './App.css';
+import MovieCard from './components/MovieCards'; // Ensure this path matches your file structure
 
 function App() {
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <MovieCard movie={{title :'Harry Potter' , release :'12/05/2007'}} />
     </div>
   );
 }
---
+++ src/components/MovieCards.jsx
@@ -0,0 +1,31 @@
+function MovieCard({movie}){
+    function onFavoriteClick (){
+        alert("Clicked ")
+    }
+    return (
+<div className="movie-card" >
+    <div className="movie-poster" >
+        <img src={movie.url} alt="movie" ></img>
+        <div className="movie-overlay">
+            <button className="favorite-btn" onClick={onFavoriteClick}>
+            ♡
+            </button>
+
+        </div>
+
+    </div>
+    <div className="movie-info">
+        <h3> {movie.title} </h3>
+        <p>{movie.release}</p>
+
+    </div>
+
+
+
+</div>
+
+
+    )
+}
+
+export default MovieCard ; 
